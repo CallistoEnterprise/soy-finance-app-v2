a0819cd096027f4e8377fff8112f1b42f3e77d8c
Yuriy <yuriy77k@gmail.com>
2024-02-26 22:47:35 +0200

    update migration contract

--- src/config/addresses/migration.ts
+++ src/config/addresses/migration.ts
@@ -1,3 +1,3 @@
-export const migrateContractAddress = "0x346984a5a13241dAf2587571Ce7D86cEA77bfB7e";
+export const migrateContractAddress = "0x6dC62C4E591f2A8638f160002e1AABdc5Ea7256A";
 export const IDOMigrateContractAddress = "0x70F340045613F86D6c5CEb787B5DA28B765c2470";
 export const slothVestingContract = "0x98194aaA67638498547Df929DF4926C7D0DCD135";
